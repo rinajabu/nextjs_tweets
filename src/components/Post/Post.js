65235344be92e6a7ba323bebdd832e4935b5ca85
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro.local>
2021-09-28 12:18:55 -0400

    post component made

---
+++ src/components/Post/Post.js
@@ -0,0 +1,16 @@
+import styles from './Post.module.scss'
+
+const Post = ({ content, date }) => {
+    return (
+        <>
+            <p className={styles.postContent}>
+                { content }
+            </p>
+            <p className={styles.postDate}>
+                { date }
+            </p>
+        </>
+    )
+}
+
+export default Post
